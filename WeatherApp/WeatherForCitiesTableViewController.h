6480c1de852d14433edcfb992a714d91ca72b0a0
Nodir <sinigami83@mail.ru>
2018-12-25 13:40:11 +0500

    added ViewTableController class  all cities and for the city

---
+++ WeatherApp/WeatherForCitiesTableViewController.h
@@ -0,0 +1,10 @@
+//
+//  Created by Nodir Latipov on 12/25/18.
+//  Copyright © 2018 Home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WeatherForCitiesTableViewController : UITableViewController
+
+@end
